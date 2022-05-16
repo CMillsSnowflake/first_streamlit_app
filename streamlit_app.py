29f1e422287e5ecdfdd2c5430bca5f237ef494fa
Charmain Mills <105654689+CMillsSnowflake@users.noreply.github.com>
2022-05-16 16:50:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,9 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.title('Breakfast Menu')
 
-print("Omega 3 & Blueberry Oatmeal")
-
+# print("Omega 3 & Blueberry Oatmeal")
+# print does not work
+'test'
 # Menu_items = [['Omega 3 & Blueberry Oatmeal'],['Kale, Spinach & Rocket Smoothie'],[;Hard-Boiled Free-Range Egg']]
                                                                                    
 # def get_first(concept);                                                                                   
