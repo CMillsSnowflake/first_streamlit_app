48b3391f58c93f49be351250e26b846e102fdd6a
Charmain Mills <105654689+CMillsSnowflake@users.noreply.github.com>
2022-05-16 17:00:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,6 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-#item2 = 
-'Kale, Spinach & Rocket Smoothie'
-#item3 = 
-'Hard-Boiled Free-Range Egg'
-
-#print(item1)
-#print(item2)
-#print(item3)
-# print not working
-print("Hello,World!")                                                                                   
-# def get_first(concept);                                                                                   
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
