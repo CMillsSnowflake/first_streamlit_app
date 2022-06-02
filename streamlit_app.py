0a2a04996ba5e6c8a1faa292f1cdf89c1b7a8ae6
Charmain Mills <105654689+CMillsSnowflake@users.noreply.github.com>
2022-06-02 15:09:11 +0200

    Update streamlit_app.py
    Put All the Import Commands Together, At the Top
    Convert the Old Imports to Comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Mom''s New Healthy Diner')
 
@@ -10,23 +14,23 @@ streamlit.text('🥑🍞 Avocardo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
@@ -38,7 +42,7 @@ streamlit.dataframe(fruityvice_normalised)
 # don't run anything past here while we troublesheet
 streamlin.stop()
 
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
